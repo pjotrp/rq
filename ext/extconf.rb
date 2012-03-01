34ed53c815be7c9279abb4a3f34b1c8916653e3d
Francesco Strozzi <francesco.strozzi@gmail.com>
2012-03-01 17:11:26 +0100

    SQLite 2.8.17 now is bundled within the gem

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,8 +1,20 @@
 require 'mkmf'
 
-dir_config( "sqlite4rq" )
-have_library( "sqlite4rq" )
-if have_header( "sqlite.h" ) and have_library( "sqlite", "sqlite_open" )
+path = File.expand_path(File.dirname __FILE__)
+sqlite_dir = "sqlite-2.8.17"
+path_to_sqlite = path+"/"+sqlite_dir
+
+Dir.chdir path_to_sqlite
+system "./configure --prefix="+path_to_sqlite
+system "make && make install"
+
+Dir.chdir path
+
+if (find_library("sqlite","sqlite_open",path_to_sqlite+"/lib") and
+    find_library("sqlite","main",path_to_sqlite+"/lib") and 
+    find_header("sqlite.h",path_to_sqlite+"/include"))
   create_makefile( "_sqlite4rq" )
 end
+
+
 
